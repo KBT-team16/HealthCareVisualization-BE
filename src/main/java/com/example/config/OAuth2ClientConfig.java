b4575cffba8a3fba0e559865939dfbb1c16261b3
supportlaver <ak4686@naver.com>
2024-08-09 10:17:17 +0900

    feat: OAuth2 Model 생성

---
+++ src/main/java/com/example/config/OAuth2ClientConfig.java
@@ -0,0 +1,32 @@
+package com.example.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@EnableWebSecurity
+@Configuration
+public class OAuth2ClientConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/static/js/**" ,
+                "/static/images/**" , "/static/css/**","/static/scss/**");
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(authRequest -> authRequest
+                .requestMatchers(new AntPathRequestMatcher("/")).permitAll());
+
+        http.oauth2Login(Customizer.withDefaults());
+        http.logout(logout -> logout.logoutSuccessUrl("/"));
+        return http.build();
+    }
+}
